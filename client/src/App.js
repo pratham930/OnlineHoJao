9014725321f0c7fe0f86b4e3eac5d736598512e6
pratham930 <priyatam.patel17@gmail.com>
2022-12-25 02:13:53 -0800

    notjing changed
    notjing changed

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import 'bulma/css/bulma.min.css';
+import Footer from './pages/Footer';
+import NavBar from './pages/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+  raju
+  <NavBar/>
+  <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Footer.js
@@ -0,0 +1,137 @@
+import { Container } from '@mui/material'
+import { Box } from '@mui/system'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+<Box className="is-flex is-justify-content-space-evenly has-background-black">
+
+<div class="columns is-gapless  is-multiline has-text-white  is-mobile ">
+  <div class="column is-one-quarter">
+    is-one-quarter
+  </div>
+  <div class="column is-one-quarter">
+    is-one-quarter
+  </div>
+  <div class="column is-one-quarter">
+    is-one-quarter
+  </div>
+  <div class="column is-one-quarter">
+    is-one-quarter
+  </div>
+  <div class="column is-half">
+    is-half
+  </div>
+  <div class="column is-one-quarter">
+    is-one-quarter
+  </div>
+  <div class="column is-one-quarter">
+    is-one-quarter
+  </div>
+  <div class="column is-one-quarter">
+    is-one-quarter
+  </div>
+  <div class="column">
+    Auto
+  </div>
+</div>
+</Box>
+
+    {/* <div className='is-justify-content-space-between'>
+
+<div className="columns has-text-white py-1 pl-6 has-background-black">
+<Container>
+      
+  <div className="column">
+    <Box>
+       services
+    </Box>
+  </div>
+  <div className="column">
+  
+  <Box>
+        services
+    </Box></div>
+  <div className="column">
+  <Box>
+        services
+    </Box></div>
+  <div className="column">
+  <Box>
+        services
+    </Box></div>
+</Container>
+<Container>
+      
+  <div className="column">
+    <Box>
+       services
+    </Box>
+  </div>
+  <div className="column">
+  
+  <Box>
+        services
+    </Box></div>
+  <div className="column">
+  <Box>
+        services
+    </Box></div>
+  <div className="column">
+  <Box>
+        services
+    </Box></div>
+</Container>
+<Container>
+      
+  <div className="column">
+    <Box>
+       services
+    </Box>
+  </div>
+  <div className="column">
+  
+  <Box>
+        services
+    </Box></div>
+  <div className="column">
+  <Box>
+        services
+    </Box></div>
+  <div className="column">
+  <Box>
+        services
+    </Box></div>
+</Container>
+<Container>
+      
+  <div className="column">
+    <Box>
+       services
+    </Box>
+  </div>
+  <div className="column">
+  
+  <Box>
+        services
+    </Box></div>
+  <div className="column">
+  <Box>
+        services
+    </Box></div>
+  <div className="column">
+  <Box>
+        services
+    </Box></div>
+</Container>
+</div>
+
+
+    </div> */}
+    </>
+
+  )
+}
+
+export default Footer
